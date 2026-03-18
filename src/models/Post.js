1a3de2f186d73f3c1a5359d7efe2ac12cb68126f
rezaulkarim <rezaulrahaat@gmail.com>
2026-03-18 21:12:04 +0600

    fix: remove redundant indexing for tags and statuses

--- src/models/Post.js
+++ src/models/Post.js
@@ -311,8 +311,6 @@ postSchema.index({ createdAt: -1 });
 postSchema.index({ type: 1, createdAt: -1 });
 postSchema.index({ isPrivate: 1, createdAt: -1 });
 postSchema.index({ reactionCount: -1, createdAt: -1 });
-postSchema.index({ tags: 1 });
-postSchema.index({ status: 1 });
 postSchema.index({ title: "text", content: "text", tags: "text" });
 // Partial index on acceptedComment to exclude nulls
 postSchema.index({ acceptedComment: 1 }, { partialFilterExpression: { acceptedComment: { $ne: null } } });
